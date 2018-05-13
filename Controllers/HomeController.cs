d5056e9cef62d7534aa66521bfde8c19d50145a7
Tuấn Hin <minhtuan200990@gmail.com>
2018-05-13 14:36:09 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Billing.Core.Models;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace Billing.Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // var oracle = new TM.Core.Connection.Oracle("VNPTBK");
+            // ViewBag.data = oracle.Connection.Query("SELECT * FROM J_PURCHASEORDER");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Billing.Core {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            //services.AddMvc();
+            //Add service for accessing current HttpContext AND ActionContext
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor> ();
+            services.AddSingleton<Microsoft.AspNetCore.Mvc.Infrastructure.IActionContextAccessor, Microsoft.AspNetCore.Mvc.Infrastructure.ActionContextAccessor> ();
+            services.Configure<CookiePolicyOptions> (options => {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            //Add service for accessing current HttpContext
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            // Add framework services.
+            // services.AddMvc (
+            //     config => {
+            //         config.Filters.Add (new MiddlewareFilters.Auth ());
+            //     });
+            //Filters Auth
+            //services.AddScoped<MiddlewareFilters.Auth> ();
+            //Service Context
+            //services.AddDbContext<TMShopsCore.Models.TMShopsContext> (options => options.UseSqlServer (Configuration.GetConnectionString ("MainConnection"), b => b.UseRowNumberForPaging ()));
+            services.AddDistributedMemoryCache (); // Adds a default in-memory implementation of IDistributedCache
+            services.AddSession ();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            //Add service for accessing current HttpContext AND ActionContext
+            TM.Core.Helper.TMAppContext.Services = app.ApplicationServices;
+            //CultureInfo Defalut
+            TM.Core.Format.Formating.CultureInfo();
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
+                app.UseHsts ();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles ();
+            app.UseSession();
+            app.UseCookiePolicy ();
+
+            app.UseMvc (routes => {
+                routes.MapRoute (
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
